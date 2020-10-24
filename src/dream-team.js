c5ca8d5134f7ad64b81ca627a752d7aadc2674d1
beafraidnotlearning <beafraidnotlearning@mail.ru>
2020-10-24 12:39:16 +0300

    feat: complete dream-team task from the basic-js tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  return Array.isArray(members) ? members
+        .filter((element) => typeof element == "string")
+        .map((element) => element.replace(/ /g, "").slice(0, 1).toUpperCase())
+        .sort()
+        .join("")
+    : false;
 };
