f73877e07c4417b21c66829bb2254a5ce5d88efa
beafraidnotlearning <beafraidnotlearning@mail.ru>
2020-10-24 12:42:40 +0300

    feat: complete transform-array task from the basic-js tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(array) {
+  if (!Array.isArray(array)) {
+    throw new Error();
+  }
+  const result = [];
+  const keywords = ["--discard-next", "--discard-prev", "--double-next", "--double-prev"];
+  for (let index = 0; index < array.length; index++) {
+    switch (array[index]) {
+      case keywords[0]:
+        if (index < array.length - 1) {
+          index++;
+        }
+        break;
+      case keywords[1]:
+        if (index > 0 && array[index - 2] != keywords[0]) {
+          result.pop();
+        }
+        break;
+      case keywords[2]:
+        if (index < array.length - 1) {
+          result.push(array[index + 1]);
+        }
+        break;
+      case keywords[3]:
+        if (index > 0 && array[index - 2] != keywords[0]) {
+          result.push(array[index - 1]);
+        }
+        break;
+      default:
+        result.push(array[index]);
+    }
+  }
+  return result;
 };
